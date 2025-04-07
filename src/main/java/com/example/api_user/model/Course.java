2a72086eccf0ad1e935af61b75d0adf05d466f54
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-04-07 13:36:06 -0300

    add model Course

---
+++ src/main/java/com/example/api_user/model/Course.java
@@ -0,0 +1,42 @@
+package com.example.api_user.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @ManyToMany(mappedBy = "courses")
+    @JsonIgnore
+    private Set<Student> students;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+}
